a4bf4e0fada598ab38fb9a8d2724bb70a5025290
Vitukaa <vitalija.dau@gmail.com>
2023-03-12 18:35:16 +0200

    Add create user form

--- src/pages/CreateUserPage.js
+++ src/pages/CreateUserPage.js
@@ -1,74 +1,125 @@
 import React, { useState } from 'react'
 
 export default function CreateUserPage() {
-  const [formData, setFormData] = useState({
-    name: '',
-    username: '',
-    email: '',
-})
+    const defaultFormData = {
+        name: '',
+        username: '',
+        description: '',
+        email: '',
+        phone: '',
+        address: '',
+        website: '',
+        company: '',
+    }
 
+  const [formData, setFormData] = useState(defaultFormData)
 
-  const formInputHandler = (event) => {
-    setFormData(prevState => {
-        const newData = {...prevState}
-        newData[event.target.name] = event.target.value
-        return newData
-    })
-}
+  const [userCreated, setUserCreated] = useState(false)
 
-const newUserHandler = () => {
-  fetch(`http://localhost:3000/users/`, {
-      method: 'POST',
-      body: JSON.stringify(
-          {...formData}
-      ),
-      headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-      },
-  })
-  .then((response) => response.json())
-  .then((json) => console.log(json));
-}
+  const [errorMessages, setErrorMessages] = useState([])
+
+    const validateForm = () => {
+        let messages = []
+
+        if (!formData.name) {
+            messages.push('Name is required')
+        }
+        if (!formData.username) {
+            messages.push('Username is required')
+        }
+        if (!formData.description) {
+            messages.push('Description is required')
+        }
+        if (!formData.email) {
+            messages.push('Email is required')
+        }
+        if (!formData.address) {
+            messages.push('Address is required')
+        }
+
+        if (messages.length === 0) {
+            return true
+        } else {
+            setErrorMessages(messages.reduce((str, current) => str + '; ' + current))
+            return false
+        }
+    }
+
+    const formInputHandler = (event) => {
+        setFormData(prevState => {
+            const newData = {...prevState}
+            newData[event.target.name] = event.target.value
+            return newData
+        })
+    }   
+
+    const newUserHandler = (event) => {
+        event.preventDefault()
+
+        if (!validateForm()) {
+        return
+        }
+
+        fetch(`http://localhost:3000/users/`, {
+        method: 'POST',
+        body: JSON.stringify(
+            {...formData}
+            ),
+            headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            },
+        })
+        .then((response) => response.json())
+        .then((json) => console.log(json));
+
+        setFormData(defaultFormData)
+        setUserCreated(true)
+    }
 
 
   return (
     <div>
-      <form onSubmit={newUserHandler}>
+          <form onSubmit={newUserHandler}>
         <div className='form-control'>
-          <label htmlFor='name'>*Full name:</label>
-          <input type='text' name='name' value={formData.name} onChange={formInputHandler}></input>
+            <label htmlFor='name'>*Full name:</label>
+            <input type='text' name='name' value={formData.name} onChange={formInputHandler}></input>
         </div>
         <div className='form-control'>
-          <label htmlFor='username'>*Username:</label>
-          <input type='text' name='username' value={formData.username} onChange={formInputHandler}></input>
+            <label htmlFor='username'>*Username:</label>
+            <input type='text' name='username' value={formData.username} onChange={formInputHandler}></input>
         </div>
         <div className='form-control'>
-          <label htmlFor='description'>*Description:</label>
-          <textarea name='description' rows="8" cols="50" value={formData.description} onChange={formInputHandler}></textarea>
+            <label htmlFor='description'>*Description:</label>
+            <textarea name='description' rows="8" cols="50" value={formData.description} onChange={formInputHandler}></textarea>
         </div>
         <div className='form-control'>
-          <label htmlFor='email'>*Email:</label>
-          <input type='email' name='email' value={formData.email} onChange={formInputHandler}></input>
+            <label htmlFor='email'>*Email:</label>
+            <input type='email' name='email' value={formData.email} onChange={formInputHandler}></input>
         </div>
         <div className='form-control'>
-          <label htmlFor='phone'>Phone:</label>
-          <input type='tel' name='phone' value={formData.phone} onChange={formInputHandler}></input>
+            <label htmlFor='phone'>Phone:</label>
+            <input type='tel' name='phone' value={formData.phone} onChange={formInputHandler}></input>
         </div>
         <div className='form-control'>
-          <label htmlFor='address'>*Address:</label>
-          <input type='text' name='address' value={formData.address} onChange={formInputHandler}></input>
+            <label htmlFor='address'>*Address:</label>
+            <input type='text' name='address' value={formData.address} onChange={formInputHandler}></input>
         </div>
         <div className='form-control'>
-          <label htmlFor='website'>Website:</label>
-          <input type='text' name='website' value={formData.website} onChange={formInputHandler}></input>
+            <label htmlFor='website'>Website:</label>
+            <input type='text' name='website' value={formData.website} onChange={formInputHandler}></input>
         </div>
         <div className='form-control'>
-          <label htmlFor='company'>Company:</label>
-          <input type='text' name='company' value={formData.company} onChange={formInputHandler}></input>
+            <label htmlFor='company'>Company:</label>
+            <input type='text' name='company' value={formData.company} onChange={formInputHandler}></input>
         </div>
             
-          <input type='submit' value='Create new user'></input>
-        </form>
+        <input type='submit' value='Create new user'></input>
+    </form>
+    {userCreated ? (
+        <h2>New user ({formData.name}) was created!</h2>
+    ) : (
+        <p>{errorMessages}</p>
+    )}
     </div>
   )
 }

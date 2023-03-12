aa5a74a9d13b79c9606d92ccaaf43447dafe2e87
Vitukaa <vitalija.dau@gmail.com>
2023-03-12 18:58:36 +0200

    Add delete user button in single user page

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -4,8 +4,8 @@ import { useLocation, useParams } from 'react-router-dom'
 export default function UserPage() {
     const { userId } = useParams()
 
-    const [loading, setLoading] = useState(true)
     const [user, setUser] = useState('')
+    const [isDeleted, setIsDeleted] = useState(false)
     
     useEffect(() => {
         fetch(`http://localhost:3000/users/${userId}`)
@@ -15,6 +15,14 @@ export default function UserPage() {
                 setUser(userData)
             })
     }, [])
+
+    const deleteUserHandler = () => {
+        fetch(`http://localhost:3000/users/${userId}`, {
+            method: 'DELETE',
+        });
+
+        setIsDeleted(true)
+    }
     
     console.log(user)
 
@@ -25,31 +33,37 @@ export default function UserPage() {
   return (
     <div>
         {user &&
-        <>
-<h1>{user.name} ({user.username})</h1>
-            <h3>About me:</h3>
-            <p>{user.description}</p>
-            <h3>Contacts:</h3>
-            <ul>
-                <li>{user.email}</li>
-                {user.address && (
-                    <li>Address: <a target='blank' href={`http://maps.google.com/?q=${user.address.street},${user.address.suite},${user.address.city}`}>{user.address.street}, {user.address.suite}, {user.address.city}</a></li>
-                )}
-                <li>{user.phone}</li>
-            </ul>
-            {user.company &&
-                <h4>Working at: {user.company.name}</h4>
-            }
-            <h5>Additional information:</h5>
-                <ul>
-                    <li>Website: <a href={user.website} target='blank'>{user.website}</a></li>
-                    {user.interests && (
-                        <li>Interests: {user.interests}</li>
-                    )}
-                </ul>
-        </>
+        <div>
+            {!isDeleted ? (
+                <>
+                    <button className='delete-button' onClick={deleteUserHandler}>Delete user</button>
+                    <h1>{user.name} ({user.username})</h1>
+                    <h3>About me:</h3>
+                    <p>{user.description}</p>
+                    <h3>Contacts:</h3>
+                    <ul>
+                        <li>{user.email}</li>
+                        {user.address && (
+                            <li>Address: <a target='blank' href={`http://maps.google.com/?q=${user.address.street},${user.address.suite},${user.address.city}`}>{user.address.street}, {user.address.suite}, {user.address.city}</a></li>
+                            )}
+                        <li>{user.phone}</li>
+                    </ul>
+                    {user.company &&
+                        <h4>Working at: {user.company.name}</h4>
+                    }
+                    <h5>Additional information:</h5>
+                    <ul>
+                        <li>Website: <a href={user.website} target='blank'>{user.website}</a></li>
+                        {user.interests && (
+                            <li>Interests: {user.interests}</li>
+                            )}
+                    </ul>
+                </>
+            ) : (
+                <p>User ({user.name}) deleted successfully</p>
+            )}
+        </div>
         }
-            
     </div>
   )
 }

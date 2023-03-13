18c5c8ecaec93c5e0088e3dcfac70ecd3851c00c
Vitukaa <vitalija.dau@gmail.com>
2023-03-13 14:50:18 +0200

    Create posts page

---
+++ src/pages/PostsPage.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+
+export default function PostsPage() {
+
+    const [posts, setPosts] = useState([])
+
+    useEffect(() => {
+        fetch(`http://localhost:3000/posts?_expand=user`)
+        .then(res => res.json())
+        .then(postsData => {
+            setPosts(postsData)
+        })
+    }, [])
+
+
+
+    return (
+        <div>
+            {posts && (
+                <>
+                {posts.map((post, index) => (
+                    <div className='post-wrapper' key={index}>
+                        <Link to={'/posts/' + post.id}>
+                            <h2 >{post.title}</h2>
+                            <h3>Author: {post.user.name}</h3>
+                            <p>{post.body.slice(0, 150) + '......'}</p>
+                        </Link>
+                    </div>
+                ))}
+
+                </>
+            )}
+        </div>
+    )
+}

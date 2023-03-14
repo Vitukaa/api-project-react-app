c84b580737cfff575ac5325cbb97f2c915d23276
Vitukaa <vitalija.dau@gmail.com>
2023-03-14 10:16:58 +0200

    Fix user id saving to number

--- src/pages/CreatePetPage.js
+++ src/pages/CreatePetPage.js
@@ -35,7 +35,11 @@ export default function CreatePetPage() {
         setFormData(prevState => {
             const newData = {...prevState}
             console.log(newData)
-            newData[event.target.name] = event.target.value
+            if (event.target.name === 'userId') {
+                newData[event.target.name] = Number(event.target.value)
+            } else {
+                newData[event.target.name] = event.target.value
+            }
             return newData
         })
     }
